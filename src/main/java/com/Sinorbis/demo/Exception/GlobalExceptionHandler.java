ce885d66488c1bc09d8ed22f993b16e0fcbe91ae
shikha <shikha.jain0502@gmail.com>
2023-11-21 12:41:55 +1100

    exception handling updated code clean up

---
+++ src/main/java/com/Sinorbis/demo/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.Sinorbis.demo.Exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception exception) {
+        logger.error("Unexpected error occurred", exception);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An unexpected error occurred. Please try again later.");
+    }
+}
